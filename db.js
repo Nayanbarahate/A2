f55d37cac0954eea50a154d7cab39a568b9aa5fc
Nayanbarahate <nayanbarahate11@gmail.com>
2022-08-03 18:17:39 +0530

    update

--- db.js
+++ db.js
@@ -21,5 +21,23 @@ MongoClient.connect(connectionString, async (err, db) => {
     const inserting = await database.collection("emp").insertMany(employeesData)
   
     console.log(inserting)
+
+    const tofind = await database.collection("emp").find().toArray();
+    console.log(tofind);
+
+    const findsalary = await database.collection("emp").find({"salary": {$gt: "30000"}}).toArray();
+    console.log(findsalary);
+
+    const experince = await database.collection("emp").find({"overallExp": {$gt: "1"}}).toArray();
+    console.log(experince);
+
+    const conditions = await database.collection("emp").find({$and: [{"yearGrad": {$gt: "2015"}}, {"overallExp": {$gt: "1"}}]}).toArray();
+    console.log(conditions);
+
+    const update = await database.collection("emp").updateMany({"salary": {$gt: "30000"}}, {$set: {"salary": "28000"}});
+    console.log(update);
+
+    const Delete = await database.collection("emp").deleteMany({"lastCompany": "Y"});
+    console.log(Delete);
 })
   
